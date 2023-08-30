068277c9cb38393db134fc672a379c1e60484699
Oyster Lee <oysterd3@gmail.com>
2023-08-31 06:31:15 +0800

    feat: implement `useQueue`

---
+++ src/composables/useQueue.ts
@@ -0,0 +1,69 @@
+import { reactive, ref } from 'vue';
+import { v4 as uuidv4 } from 'uuid';
+
+export const QUEUE_TYPE = {
+  vip: 'VIP',
+  normal: 'NORMAL',
+} as const;
+
+type QueueType = (typeof QUEUE_TYPE)[keyof typeof QUEUE_TYPE];
+type QueueData = {
+  id: string;
+  label: string;
+  type: QueueType;
+};
+type QueueMethods = {
+  enqueue: (type: QueueType) => void;
+  dequeue: (id: string) => null | QueueData;
+};
+
+export const useQueue = (
+  initialValue: QueueData[] = [],
+): [QueueData[], QueueMethods] => {
+  const state = reactive(initialValue);
+  const currentRunningNumber = ref(1);
+  return [
+    state,
+    {
+      enqueue: (type) => {
+        if (type === QUEUE_TYPE.normal) {
+          state.push({
+            id: uuidv4(),
+            label: `Order #${currentRunningNumber.value} (Normal)`,
+            type,
+          });
+        } else {
+          const item = {
+            id: uuidv4(),
+            label: `Order #${currentRunningNumber.value} (VIP)`,
+            type,
+          };
+          if (state.length === 0) {
+            state.push(item);
+          } else {
+            for (let i = 0; i < state.length; i++) {
+              if (state[i].type === QUEUE_TYPE.vip && i === state.length - 1) {
+                state.push(item);
+                break;
+              }
+              if (state[i].type === QUEUE_TYPE.normal) {
+                state.splice(i === 0 ? 0 : i, 0, item);
+                break;
+              }
+            }
+          }
+        }
+        currentRunningNumber.value += 1;
+      },
+      dequeue: (id) => {
+        const idx = state.findIndex((v) => v.id === id);
+        let item: null | QueueData = null;
+        if (idx >= 0) {
+          item = state[idx];
+          state.splice(idx, 1);
+        }
+        return item;
+      },
+    },
+  ];
+};

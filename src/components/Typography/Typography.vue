5540a588bf2966a03548c2a586f8d15bad563e1f
Oyster Lee <oysterd3@gmail.com>
2023-08-31 10:19:21 +0800

    feat: add `Typography`

---
+++ src/components/Typography/Typography.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="CLASS_MAP[type]">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+const CLASS_MAP = {
+  title: 'font-semibold leading-6 text-gray-900',
+  subtitle: 'text-sm leading-5 text-gray-500',
+} as const;
+
+withDefaults(
+  defineProps<{
+    type?: 'title' | 'subtitle';
+  }>(),
+  {
+    type: 'title',
+  },
+);
+</script>

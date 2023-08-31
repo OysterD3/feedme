a794c7c7a977005d8d671f9fb2f007b250052835
Oyster Lee <oysterd3@gmail.com>
2023-08-31 10:18:37 +0800

    feat: reset css styles

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './style.css';
+import '@unocss/reset/tailwind.css';
 import 'virtual:uno.css';
 
 createApp(App).mount('#app');

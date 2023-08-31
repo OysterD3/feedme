0375743d3f1d469427e2bc9326616df6d142e510
Oyster Lee <oysterd3@gmail.com>
2023-08-31 10:19:13 +0800

    feat: add `List` and `ListItem`

---
+++ src/components/List/ListItem.vue
@@ -0,0 +1,10 @@
+<template>
+  <li class="flex justify-between items-center gap-x-6 py-5 px-4 max-w-2xl">
+    <div class="flex flex-col gap-1">
+      <slot name="text" />
+    </div>
+    <div>
+      <slot name="action" />
+    </div>
+  </li>
+</template>

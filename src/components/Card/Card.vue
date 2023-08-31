5916d44aede20d3343fad1d67e292a7eeaa73367
Oyster Lee <oysterd3@gmail.com>
2023-08-31 10:19:02 +0800

    feat: add `Card` and `CardTitle`

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="rounded-md shadow bg-white w-full p-4">
+    <slot />
+  </div>
+</template>
---
+++ src/components/Card/CardTitle.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-2xl font-bold text-gray-900 mb-4">
+    <slot />
+  </h1>
+</template>
